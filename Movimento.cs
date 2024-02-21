7478f3e3d85af7631dd60363d8710cb1bb09d329
RafaelScremimLindemann <62905043+RafaelScremimLindemann@users.noreply.github.com>
2024-02-20 21:51:18 -0300

    Add files via upload

---
+++ Movimento.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Conta
+{
+    class Movimento
+    {
+        public int Conta { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; set; }
+
+//Construtor Movimento com dois argumentos.
+        public Movimento(int conta, string titular)
+        {
+            Conta = conta;
+            Titular = titular;
+        }
+//Construtor Movimento com tres argumentos.
+        public Movimento(int conta, string titular, double valorDeposito) : this(conta, titular)
+        {
+            Deposito(valorDeposito);
+        }
+        
+
+        public void Deposito(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+        }
+        
+        public void Saque(double valorSaque)
+        {
+            Saldo -= valorSaque + 5;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + Conta
+                   + ", Titular: " + Titular
+                   + ", Saldo: " + Saldo.ToString(CultureInfo.InvariantCulture);
+
+        }
+        }
+        }
+
+
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using Conta;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Feito por Rafael Scremim Lindemann 11/10/2022
+Sistema simples para inserção de dados em uma conta; Inclusão de valor de deposito;
+Saque com desconto; utilizando POO acessando outra classe.*/
+
+namespace Conta;
+
+    class Conta
+    {
+        static void Main(string[] args)
+    {
+        Movimento m;
+        
+        Console.Write("Numero conta: ");
+        int conta = int.Parse(Console.ReadLine());
+
+        Console.Write("Nome do titular: ");
+        string titular = Console.ReadLine();
+
+        Console.Write("Havera deposito (s/n)?: ");
+        char cond = char.Parse(Console.ReadLine()); 
+        if (cond == 's')
+        {
+            Console.Write("Qual o valor do deposito: ");
+            double depositoInicial = double.Parse(Console.ReadLine());
+            m = new Movimento(conta, titular, depositoInicial);
+        }else
+        {
+            m = new Movimento(conta,titular);
+        }
+
+        Console.WriteLine(m);
+
+        Console.WriteLine();
+        Console.Write("Entre com deposito: ");
+        double valorDeposito = double.Parse(Console.ReadLine());
+        m.Deposito(valorDeposito);
+        Console.WriteLine(m);
+
+        Console.WriteLine();
+        Console.Write("Saque com 5 reais de desconto: ");
+        double valorSaque = double.Parse(Console.ReadLine());
+        m.Saque(valorSaque);
+        Console.WriteLine("Seu saldo é de: "+ m);
+
+        Console.WriteLine();
+        Console.WriteLine("Pressione ENTER para sair...");
+        Console.Read();
+    }
+}
+
